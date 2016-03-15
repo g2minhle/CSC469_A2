cdffb09b76b33b94a4034a36c0880bba16a12723
g2minhle <lehoangminh@live.com>
2016-03-15 00:24:49 -0400

    Add mm_init, allocate_thread_meta and allocate_mem_block

--- allocators/a2alloc/a2alloc.c
+++ allocators/a2alloc/a2alloc.c
@@ -8,6 +8,9 @@
 // Superblock size in bytes
 // TODO: find out the sb_size
 #define SUPERBLOCK_SIZE 8
+#define TRUE 1
+#define FALSE 1
+#define SUPER_BLOCK_ALIGNMENT sizeof(struct mem_block) + sizeof(struct superblock)
 
 // ======================= Flags =======================
 
@@ -28,6 +31,7 @@
 
 struct allocator_meta {
   pthread_mutex_t mem_lock;
+  struct mem_block* first_mem_block;
   struct thread_meta* heap_list;
   struct thread_meta* global_heap;
 };
@@ -56,6 +60,9 @@ struct superblock {
   struct superblock* previous;
   struct thread_meta* thread_heap;
 };
+
+
+struct allocator_meta*  mem_allocator;
 
 /* The mm_malloc routine returns a pointer to an allocated region of at least
  * size bytes. The pointer must be aligned to 8 bytes, and the entire
@@ -139,6 +146,113 @@ void mm_free(void *ptr)
 }
 
 
+struct mem_block* allocate_mem_block(struct mem_block* first_mem_block,
+                                      size_t size) {
+  struct mem_block* result_mem_block = NULL;
+  struct mem_block* previous_mem_block = NULL;
+  pthread_mutex_lock(&(mem_allocator->mem_lock));
+  struct mem_block* current_mem_block = first_mem_block;
+  while (
+    current_mem_block && (
+      current_mem_block->blk_size < size
+      || !GET_FREE_BIT(current_mem_block->flags)
+    )
+  ) {
+    /* All we are looking for is a free space that is larger or equal to
+     * the size we are looking for.
+     * we dont create about aglinment since we make sure that happend
+     */
+    previous_mem_block = current_mem_block;
+    current_mem_block = current_mem_block->next;
+  }
+
+  if(current_mem_block) {
+    // Found a memory block to be used
+    result_mem_block = current_mem_block;
+    if(size != result_mem_block->blk_size) {
+      uint32_t used_space = size - sizeof(struct mem_block);
+      // create a new free mem block
+      struct mem_block* new_mem_block = (struct mem_block*)
+        (char*)result_mem_block + used_space;
+      SET_FREE_BIT(first_mem_block->flags);
+      CLEAR_LARGE_BIT(first_mem_block->flags);
+      new_mem_block->blk_size = result_mem_block->blk_size - used_space;
+      new_mem_block->next = result_mem_block->next;
+      new_mem_block->previous = result_mem_block;
+      result_mem_block->next = new_mem_block;
+    }
+  } else {
+    // reached the last block, with no available memory
+    if (GET_FREE_BIT(previous_mem_block->flags)){
+      result_mem_block = previous_mem_block;
+      // Expand the memory
+      void* result = mem_sbrk(size - result_mem_block->blk_size);
+      if (result == NULL) return NULL;
+      // Extend the mem_block size
+      result_mem_block->blk_size = size;
+    } else {
+      // Expand the memory
+      void* result = mem_sbrk(size + sizeof(struct mem_block));
+      if (result == NULL) return NULL;
+      // Create a new free mem block
+
+      struct mem_block* new_mem_block = (struct mem_block*)(
+        (char*)previous_mem_block
+        + sizeof(struct mem_block)
+        + previous_mem_block->blk_size
+      );
+      CLEAR_LARGE_BIT(first_mem_block->flags);
+      new_mem_block->blk_size = size;
+
+      new_mem_block->next = NULL;
+      new_mem_block->previous = previous_mem_block;
+
+      previous_mem_block->next = new_mem_block;
+      result_mem_block = new_mem_block;
+    }
+  }
+  CLEAR_FREE_BIT(result_mem_block->flags);
+  pthread_mutex_unlock(&(mem_allocator->mem_lock));
+  return result_mem_block;
+}
+
+uint32_t find_total_size_need(size_t size, size_t multiplier) {
+  uint32_t total_space_including_mem_block = size + sizeof(struct mem_block);
+  uint32_t block_count = total_space_including_mem_block / multiplier;
+  // usable memory
+  uint32_t result = multiplier *  block_count;
+  if (result < total_space_including_mem_block) {
+    // exact that space or extra
+    result += multiplier;
+  }
+  result -= sizeof(struct mem_block);
+  return result;
+}
+
+
+struct thread_meta* allocate_thread_meta(pid_t thread_id) {
+  // Allocate memory for the global heap meta structure
+  uint32_t total_size_need =  find_total_size_need(
+    sizeof(struct thread_meta),
+    SUPER_BLOCK_ALIGNMENT
+  );
+
+  struct mem_block* new_mem_block = allocate_mem_block(
+        mem_allocator->first_mem_block,
+        total_size_need
+  );
+
+  if (new_mem_block == NULL) return NULL;
+
+  struct thread_meta* result = (struct thread_meta*)(
+    (char*)new_mem_block + sizeof(struct mem_block)
+  );
+  pthread_mutex_init(&result->thread_lock, NULL);
+  result->thread_id = 0;
+  return result;
+}
+
+
 /* Before calling mm_malloc or mm_free, the application program calls mm_init
  * to perform any necessary initializations, including the allocation of the
  * initial heap area. The return value should be -1 if there was a problem
@@ -147,8 +261,54 @@ void mm_free(void *ptr)
 int mm_init(void)
 {
 	if (dseg_lo == NULL && dseg_hi == NULL) {
-		return mem_init(); // mem_init ret: -1 if there was a problem, 0 otherwise
+  	// mem_init ret: -1 if there was a problem, 0 otherwise
+	  if (mem_init() == -1 ) return -1;
+
+	  /*
+	   * Consider allocate around 5 to 10 superblock in the begining
+	   * because the bencmarker will go multithread right away.
+	   * need space for you preserved data as well
+	   */
+    void* result = mem_sbrk (
+      sizeof(struct allocator_meta)
+      + SUPER_BLOCK_ALIGNMENT * 10
+    );
+
+    if (result == NULL) return -1;
+
+	  mem_allocator = (struct allocator_meta*)dseg_lo;
+	  mem_allocator->heap_list = NULL;
+    pthread_mutex_init(&mem_allocator->mem_lock, NULL);
+    mem_allocator->first_mem_block = (struct mem_block*)(
+	    (char*)mem_allocator + sizeof(struct allocator_meta)
+    );
+
+	  struct mem_block* first_mem_block = mem_allocator->first_mem_block;
+
+    SET_FREE_BIT(first_mem_block->flags);
+    CLEAR_LARGE_BIT(first_mem_block->flags);
+
+    first_mem_block->next = NULL;
+    first_mem_block->previous = NULL;
+
+    // Total space we have
+    first_mem_block->blk_size = dseg_hi - dseg_lo + 1;
+    // Minus the preserve space
+    first_mem_block->blk_size -= sizeof (struct allocator_meta);
+    // Minus the the memblock itself
+    first_mem_block->blk_size -= sizeof (struct mem_block);
+
+
+    mem_allocator->global_heap = allocate_thread_meta(0);
+
+    if (mem_allocator->global_heap == NULL) return -1;
+
+    // Init the global heap meta structure
+    mem_allocator->global_heap->next = NULL;
+
+		return 0;
 	}
-  return 0; // It's already been initialized
+	// It's already been initialized
+  return 0;
 }
 
